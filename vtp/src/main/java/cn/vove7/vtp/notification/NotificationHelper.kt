3407312b474d793995aa85cd66aae581de38e364
Vove <1132412166@qq.com>
2018-12-21 14:28:14 +0800

    通知相关

--- vtp/src/main/java/cn/vove7/vtp/notification/NotificationHelper.kt
+++ vtp/src/main/java/cn/vove7/vtp/notification/NotificationHelper.kt
@@ -6,6 +6,7 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.media.RingtoneManager
 import android.os.Build
 import java.util.*
 
@@ -16,7 +17,7 @@ import java.util.*
  * //TODO :
  * Created by Vove on 2018/7/26
  */
-class NotificationHelper(var context: Context, val channel: NotificationChannel? = null) {
+class NotificationHelper(var context: Context, val channel: NotificationChannel? = null, val alert: Boolean = false) {
 
     private lateinit var builder: Notification.Builder
     private lateinit var notificationManager: NotificationManager
@@ -62,6 +63,7 @@ class NotificationHelper(var context: Context, val channel: NotificationChannel?
                 .setContentText(content)
                 .setContentTitle(title)
                 .setAutoCancel(true)
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && icons.largeIcon != null) {
             builder.setLargeIcon(icons.largeIcon)
         }
@@ -70,13 +72,18 @@ class NotificationHelper(var context: Context, val channel: NotificationChannel?
                     .getBroadcast(context, 0, broadcastIntent, PendingIntent.FLAG_UPDATE_CURRENT)
             builder.setContentIntent(contentIntent)
         }
-        val notification = builder.build()
+        val notification = builder.build().apply {
+            if (alert && Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+                sound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM)
+                vibrate = longArrayOf(0, 200)
+            }
+        }
         notificationManager.notify(nId, notification)
     }
 
-    fun sendNewNotification(title: String, content: String,icons: NotificationIcons, broadcastIntent: Intent? = null) {
+    fun sendNewNotification(title: String, content: String, icons: NotificationIcons, broadcastIntent: Intent? = null) {
         val nId = Random().nextInt()
-        showNotification(nId, title, content,icons, broadcastIntent)
+        showNotification(nId, title, content, icons, broadcastIntent)
     }
 
     /**
